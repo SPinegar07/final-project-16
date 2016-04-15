a8f9645b3b7baa55337d16742f05f4404bfb9997
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-04-15 12:06:53 -0500

    added header, footer, and loop

--- index.php
+++ index.php
@@ -5,19 +5,21 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
 </head>
 <body>
-    <div class="container">
-
-  <div class="row">
-    <div class="six columns">Content for 1/2 width column</div>
-    <div class="six columns">Content for 1/2 width column</div>
-  </div>
-
-  <div class="row">
-    <div class="four columns">Content for 1/3 width column</div>
-    <div class="five columns">Content for 5/12 width column</div>
-    <div class="three columns">Content for 1/4 width column</div>
-  </div>
-
-</div>
+     <div class="container">
+            <?php get_header(); ?>
+    <section class="row">
+        <div class="twelve columns">
+<!-- BEGIN LOOP -->
+            <?php 
+            if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post(); 
+                } // end while
+            } // end if
+            ?>
+<!-- END LOOP -->
+        </div>
+    </section>
+<?php get_footer(); ?>
 </body>
 </html>
