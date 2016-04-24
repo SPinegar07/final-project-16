c9c57eec03b2c08ae0e54a2b0a966b176dc7e0e8
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-04-24 17:02:57 -0500

    styled menu

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 </head>
 <body>
     <div class="container">
+        <div class="row">
             <div class="twelve columns">
                 <div class="two columns">
                     <div class="right">
@@ -35,14 +36,25 @@
                         </table>
                     </div>
                 </div>
+            </div>
+        </div>
+                <div class="row">
                 <div class="twelve columns">
-                <div class="ten columns">
+                <div class="six columns">
                     <!-- ADD LINK TO HOME_URL -->
                     <a href="http://www.kittensunicornsandtacos.com/UFL"><img src="http://www.kittensunicornsandtacos.com/chips/wp-content/uploads/2016/04/chip-logo.png" alt="logo" width="300px"></a>
                 </div>
-                    
-                </div>
-            
+            </div>
+                <div class="six columns">
+                    <div class="right">
+        <?php wp_nav_menu(array(
+            'sort_column' => 'menu_order', 
+            'container_class' => 'blank-menu-header'
+            ));?>
+    </div>
+</div>      
+</div>
+</div>
 </div>
 </div>
 </body>
