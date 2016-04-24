f26cc657b29500d51a3829444ec3fee88c4028a5
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-04-24 18:24:13 -0500

    styled the loop

--- index.php
+++ index.php
@@ -21,26 +21,36 @@
                     </tr>
                 </table> 
             </div>
+        </div>
+    </section>
 <!-- END MAIN IMAGE SECTION -->
+<section class="row">
             <div class="twelve columns">
-                <div class="four columns">
+                <table width="100%">
+                    <tr>
+                        <td width="33%">
                     <?php dynamic_sidebar('main-page-left-widget'); ?>
-                </div>
-                <div class="four columns">
+                        </td>
+                        <td width="33%">
                     <?php dynamic_sidebar('main-page-center-widget'); ?>
-                </div>
-                <div class="four columns">
+                </td>
+                       <td width="33%">
                     <?php dynamic_sidebar('main-page-right-widget'); ?>
-                </div>
+                </td>
+            </tr>
+        </table>
             </div>
         <div class="twelve columns">
+            <div class="post-home">
 <!-- BEGIN LOOP -->
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 <h1><?php the_title(); ?></h1>
 <h4>Posted on <?php the_time('F jS, Y') ?></h4>
 <p><?php the_content(__('(more...)')); ?></p>
 <hr> <?php endwhile; else: ?>
 <p><?php _e('ERROR! Could not compute! Our 1s and 0s are broken, search for something else!'); ?></p><?php endif; ?>
+</div>
 <!-- END LOOP -->
         </div>
     </section>
